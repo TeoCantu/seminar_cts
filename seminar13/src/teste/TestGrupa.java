576440886f4054ea0275285a646399bb9d6302aa
TeoCantu <teocantu@gmail.com>
2021-05-27 11:19:17 +0300

    JUnit 4

--- seminar13/src/teste/TestGrupa.java
+++ seminar13/src/teste/TestGrupa.java
@@ -1,12 +1,84 @@
 package teste;
 
 import clase.Grupa;
+import clase.Student;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class TestGrupa {
+    private Grupa grupa;
 
+    @Before
+    public void setup() {
+        grupa = new Grupa(1077);
+        for(int i=0; i<10; i++) {
+            Student student = new Student("Marcel");
+            student.adaugaNota(5);
+            student.adaugaNota(10);
+            student.adaugaNota(4);
+            grupa.adaugaStudent(student);
+        }
+    }
+
+    //right bicep pentru restante
+    // right
+    @Test
+    public void testGetPromovabilitateRight() {
+        for(int i=0; i<2; i++){
+            Student student = new Student("Maria");
+            student.adaugaNota(9);
+            grupa.adaugaStudent(student);
+        }
+        assertEquals(0.16, grupa.getPromovabilitate(), 0.05);
+    }
+
+    // boundary
+    @Test
+    public void testGetPromovabilitateLowerBoundary() {
+        assertEquals(0, grupa.getPromovabilitate(), 0.01);
+    }
+
+    @Test
+    public void testGetPromovabilitateUpperBoundary() {
+        Grupa grupa = new Grupa(1076);
+        for(int i=0; i<10; i++){
+            Student student = new Student("Maria");
+            student.adaugaNota(9);
+            grupa.adaugaStudent(student);
+        }
+
+        assertEquals(1, grupa.getPromovabilitate(), 0.01);
+
+    }
+
+    // inverse relationship - nu se aplica
+    // cross check - nu se aplica
+    // error condition - pt situatia in care nu exista studenti in grupa
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetPromovabilitateErrorCondition() {
+        Grupa grupa = new Grupa(1068);
+        grupa.getPromovabilitate();
+    }
+
+    //performance
+    @Test(timeout = 500)
+    public void testGetPromovabilitatePerformance() {
+        grupa.getPromovabilitate();
+    }
+
+    // correct pentru getPromovabilitate
+    // conformance - format returnat - testul de right
+    // ordering - nu se aplica
+    // range - intervalul de valori = right+ upperboundary+ lowerboundary
+    // reference - referinta externa este Studentul cu apelul metodei areRestanta
+    // existance - errorcondition (ca nu am studenti) + existenta rezultatului
+    // cardinality - 0,1,n = 0(errorcondition)+n(mai multi) + de facut pentru un student
+    // time - performance + daca e respectat un sablon (apel metode in ordinea corecta)
+
+    // right bicep pentru constructor
     @Test
     public void testConstructorRight() {
         Grupa grupa = new Grupa(1077);
@@ -61,5 +133,6 @@ public class TestGrupa {
         assertNotNull(grupa.getStudenti());
     }
 
-    
+
+
 }
